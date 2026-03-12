5e8bb9711bf6afc02fa75090e94855451496a05b
SithuminiDulanjalee <sithumini2222@gmail.com>
2026-03-12 17:31:00 +0530

    backend module created

---
+++ AuraBloom_Backend/src/test/java/lk/ijse/aurabloom_backend/AuraBloomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.aurabloom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraBloomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
